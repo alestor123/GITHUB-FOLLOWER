28b4cb01ef9230303ae25cd1342c068de739ed82
alestor123 <alestoraldous@gmail.com>
2021-01-22 22:08:43 +0530

    added cli

--- cli.js
+++ cli.js
@@ -1,1 +1,8 @@
 #!/usr/bin/env node
+var followers = require('./App'),
+chalk = require('chalk');
+(async () => {
+var follow = await followers(process.argv[2] || 'github')
+console.log(chalk.cyanBright.bold(`${follow.uname} has ${follow.no} followers  `))
+follow.followers.map(name => console.log(chalk.bold.yellowBright(` \n ${name.login} \n`)))
+})();
